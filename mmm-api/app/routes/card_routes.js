8489e46c7e684fce1643124aeff016457e25bd28
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-09-06 23:42:55 +0300

    Add card routes, done with -read-

---
+++ mmm-api/app/routes/card_routes.js
@@ -0,0 +1,55 @@
+// Require necessary NPM Packages
+const express = require('express');
+
+// Pull in Mongoose model for Card
+const Card = require('../models/card');
+
+// Instantiate a router (mini app the only handles routes)
+const router = express.Router();
+
+/**
+ * Action:      INDEX
+ * Method:      GET
+ * URI:         /api/cards
+ * Description: Get all Cards
+ */
+router.get('/api/articles', (req, res) => {
+    Card.find({}, (error, cards) => {
+        // Return all cards
+        if (!error) {
+            res.status(200).json({ cards: cards });
+        } else {
+            // if there are any errors 
+            res.status(500).json({ error: error });
+        }
+    });
+});
+
+/**
+ * Action:      SHOW
+ * Method:      GET
+ * URI:         /api/cards/:id
+ * Description: Get a Card by Card ID
+ */
+router.get('/api/cards/:id', (req, res) => {
+    Card.findById(req.params.id, (error, card) => {
+        if (!error) {
+            if (card) {
+                // Return the card
+                res.status(200).json({ card: card });
+            } else {
+                // if there are no card with a matching ID
+                res.status(404).json({
+                    error: {
+                        name: 'DocumentNotFoundError',
+                        message: 'The provided Id doesn\'t match any documents'
+                    }
+                })
+            }
+        } else {
+            //if there are any errors
+            res.status(500).json({ error: error });
+        }
+    });
+});
+
