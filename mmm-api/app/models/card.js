28969618e8283f9bd479476a1ec2d4f2badd63c8
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-09-06 23:02:16 +0300

    Add Card model

---
+++ mmm-api/app/models/card.js
@@ -0,0 +1,22 @@
+// Require necessary NPM Packages
+const mongoose = require('mongoose');
+
+// Define Card Schema
+const cardSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+        timestamps: true
+});
+
+// Compile model based on the schema
+const Card = mongoose.model('Card', cardSchema);
+
+// Export the model
+module.exports = Card;
