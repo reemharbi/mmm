e432d9d824cf5f07809441b8ec646f1613c4a932
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-09-09 16:59:28 +0300

    Fix Bug: the userId was global variable which led to removal of the last player when any player is disconnected

--- mmm-api/game.js
+++ mmm-api/game.js
@@ -4,26 +4,28 @@ const socketIO = require("socket.io");
 // Database Models
 const Player = require('./app/models/player');
 
-let io;
-let gameSocket;
-let userId;
+
 
 //  Function Called when Player is created
 exports.initGame = (sio, socket) => {
-    io = sio;
-    gameSocket = socket;
-    userId = socket.handshake.query.userId;
+    const io = sio;
+    const gameSocket = socket;
+    const userId = gameSocket.handshake.query.userId;
     console.log(`User is connected ${userId}`);
+
+    // When player is disconnected remove their document from DB
+    playerDisconnect = () => {
+        console.log("User is Disconnected");
+        Player.findById(userId, (error, player) => {
+            if (player) {
+                player.remove();
+            }
+        });
+    }
     gameSocket.on("disconnect", playerDisconnect);
+
+
 }
 
 
-// When player is disconnected remove their document from DB
- playerDisconnect = () => {
-    console.log("User is Disconnected");
-    Player.findById(userId, (error, player) => {
-        if(player){
-            player.remove();
-        }
-    });
-}
+
