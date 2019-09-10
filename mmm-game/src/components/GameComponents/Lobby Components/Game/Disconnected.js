eae05f5367742092a0feb2bda26e893362f658a0
reemharbi <reemharbi.3@gmail.com>
2019-09-10 18:44:02 +0300

    Created Disconnected component

---
+++ mmm-game/src/components/GameComponents/Lobby Components/Game/Disconnected.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Disconnected.css'
+
+export default class Disconnected extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
