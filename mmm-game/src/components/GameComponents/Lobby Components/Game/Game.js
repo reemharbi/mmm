476a40b6aebc4c9dffd5071cecf0e19a7c0cf4e4
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2019-09-12 03:05:26 +0300

    42

--- mmm-game/src/components/GameComponents/Lobby Components/Game/Game.js
+++ mmm-game/src/components/GameComponents/Lobby Components/Game/Game.js
@@ -6,12 +6,16 @@ import './Game.css'
 import './Game.css';
 import Waiting from './Waiting';
 import Disconnected from './Disconnected';
+import InvestorFinalPhase from './InvestorFinalPhase';
+import ProjectManagerFinalView from './ProjectManagerFinalView';
 
 export default class Game extends Component {
 
   state = {
     currentCard: null,
-    currentComponent: 'waiting'
+    currentComponent: 'waiting',
+    finalPhase: false,
+    playersInRoom: []
   }
 
 
@@ -46,6 +50,16 @@ export default class Game extends Component {
       }
     
     })
+
+    this.props.socket.on("finalPhase" , (players) => {
+      this.setState({
+        playersInRoom: players,
+        finalPhase: true
+      })
+    })
+
+
+
   }
 
 
@@ -53,10 +67,24 @@ export default class Game extends Component {
 
 
     
+    if (this.state.finalPhase) {
+      
+  if (this.state.currentComponent === 'investor') {
+    return <InvestorFinalPhase room={this.props.room} socket={this.props.socket} players={this.state.playersInRoom} card={this.state.currentCard}/>
+  }
+  else{
+    return <ProjectManagerFinalView socket={this.props.socket} players={this.state.playersInRoom} card={this.state.currentCard}/>
+
+  }
 
 
-    if (this.state.currentComponent === 'investor') {
-      return <div>
+
+
+    }
+else {
+
+  if (this.state.currentComponent === 'investor') {
+    return <div>
         <p>Current players in room: {this.props.room.players.length}</p>
         <Investor exitGame={this.props.exitGame} card={this.state.currentCard} room={this.props.room} updateRoom={this.props.updateRoom} socket={this.props.socket} />
       </div>
@@ -64,20 +92,21 @@ export default class Game extends Component {
     else if (this.state.currentComponent === 'project manager') {
       return <div>
         <p>Current players in room: {this.props.room.players.length}</p>
-        <ProjectManager exitGame={this.props.exitGame} card={this.state.currentCard} room={this.props.room} updateRoom={this.props.updateRoom} socket={this.props.socket} />
+        <ProjectManager  exitGame={this.props.exitGame} card={this.state.currentCard} room={this.props.room} updateRoom={this.props.updateRoom} socket={this.props.socket} user={this.props.user}/>
      
       </div>
     }
-
+    
     else if(this.state.currentComponent === 'waiting'){
       return <div>
         <Waiting exitGame={this.props.exitGame} room={this.props.room}/>
         </div>
 
-    }
-    else if (this.state.currentComponent === 'disconnected player'){
-      return <Disconnected />
-
-    }
-  }
+}
+else if (this.state.currentComponent === 'disconnected player'){
+  return <Disconnected />
+  
+}
+}
+}
 }
--- mmm-game/src/components/GameComponents/Lobby Components/Game/InvestorFinalPhase.js
+++ mmm-game/src/components/GameComponents/Lobby Components/Game/InvestorFinalPhase.js
@@ -1,44 +1,40 @@
 import React, { Component } from 'react'
-import { Button, Card, Grid, Image, Form } from 'semantic-ui-react'
-import './ProjectManagerView.css'
+import { Button, Card, Grid, Image } from 'semantic-ui-react'
+import './InvestorView.css'
+import './AnimatedBG.css'
 import loading from './writing.svg'
-import {updatePlayer} from '../api'
 
-export default class ProjectManagerView extends Component {
-    
+export default class InvestorFinalPhase extends Component {
+
     state = {
-        approachField: "",
-        isSubmitted: false
+        winner: ""
     }
 
-    handleChange = (event) => {
-        this.setState({
-            approachField: event.target.value
+
+    chooseWinner(winner) {
+        this.props.socket.emit("gameWinner" , winner , this.props.room)
+
+    }
+
+
+
+    componentDidMount(){
+        this.props.socket.on("gameResult" , (winner) => {
+
+            this.setState({
+                winner: `The winner is ${winner.name}!!!`
+            })
+
         })
-
     }
 
-
-    approachSubmit = (value) => {
-        value.preventDefault();
-        const body = {approach: this.state.approachField};
-
-        updatePlayer(this.props.user._id , body).then( res => {
-            console.log("approach submit",res)
-            this.props.socket.emit("submitApproach" , this.props.room)
-        } );
-this.setState({isSubmitted:true})
-
-    }
-
-
-
-
+//
 
     render() {
         return (
             <div>
-                  <Grid columns={3}>
+                <p>{this.state.winner}</p>
+    <Grid columns={3}>
     <Grid.Row style={{paddingBottom: '0px'}}>
       <Grid.Column>
             <div className='card-div'>
@@ -51,32 +47,24 @@ this.setState({isSubmitted:true})
       <Image centered style={{marginTop:'1rem'}} src='https://react.semantic-ui.com/images/wireframe/square-image.png' size='tiny' circular />
       </Grid.Column>
       <Grid.Column>
-        <Button floated='right' color='red' style={{color:'black', marginTop:'1rem', marginRight: '1rem'}} className="exit-button" onClick={this.props.exitGame}>EXIT GAME</Button>
+        <Button floated='right' color='yellow' style={{color:'black', marginTop:'1rem', marginRight: '1rem'}} className="exit-button" onClick={this.props.exitGame}>EXIT GAME</Button>
       </Grid.Column>
     </Grid.Row>
-
+//
     <Grid.Row style={{paddingBottom: '0px', paddingTop: '0px'}}>
         <Grid.Column>
             <div style={{height:'8vh'}}>
             </div>  
         </Grid.Column>
         <Grid.Column>
-            <p className='fade-in-fwd'>Phase 1</p>
+            <p className='fade-in-fwd scale-out-center'>Phase 1</p>
         </Grid.Column>
     </Grid.Row>
     <Grid.Row style={{paddingBottom: '0px', paddingTop: '0px', height:'9vh'}}>
         <Grid.Column>
-            <div>
+            <div onClick={(winner) => this.chooseWinner(this.props.players[1])}>
                 <Card style={{paddingBottom: '0px'}} className="project-card player-card">
-                {!this.state.isSubmitted &&
-                <Form> 
-                    <Form.TextArea value={this.state.approachField} onChange={(e) => this.handleChange(e)} placeholder='How would you approach this project?' />
-                    <Form.Button onClick={this.approachSubmit}>Submit</Form.Button>
-                </Form>
-            }
-                {this.state.isSubmitted && 
-                <div>{this.state.approachField}</div>
-                }
+                <div>{this.props.players[1].approach}</div>
                 </Card>
             </div>
        </Grid.Column>
@@ -85,9 +73,10 @@ this.setState({isSubmitted:true})
             </div>
        </Grid.Column>
        <Grid.Column>
-            <div>
+            <div onClick={(winner) => this.chooseWinner(this.props.players[0])}>
                 <Card style={{paddingBottom: '0px'}} className="project-card player-card p2">
-                <Image  centered src={loading} size='tiny' style={{background:'rgba(255,255,255,.5)', marginTop:'5rem'}} />
+                
+                <div>{this.props.players[0].approach}</div>
                 </Card>
             </div>
        </Grid.Column>
